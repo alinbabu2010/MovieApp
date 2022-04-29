5f460ef921e91951c90acd18489a7145d7951c97
Alin Babu <alinbabu2010@gmail.com>
2022-04-29 09:30:17 +0530

    Add Scaffold composable function

--- app/src/main/java/com/jetpack/movieapp/MainActivity.kt
+++ app/src/main/java/com/jetpack/movieapp/MainActivity.kt
@@ -3,12 +3,11 @@ package com.jetpack.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.jetpack.movieapp.ui.theme.MovieAppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +22,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RootContent(content: @Composable () -> Unit){
-    MovieAppTheme() {
+    Scaffold(topBar = {
+        TopAppBar(
+            backgroundColor = Color.Magenta,
+            elevation = 5.dp
+        ) {
+            Text(text = "Movies")
+        }
+    }) {
         content()
     }
 }

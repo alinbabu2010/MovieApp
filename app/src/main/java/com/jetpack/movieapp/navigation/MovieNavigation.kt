ed68a557d78ee47752a33598dac3ae644a2f651a
Alin Babu <alinbabu2010@gmail.com>
2022-04-29 14:29:51 +0530

    Add navigation builder for home and details screen

--- app/src/main/java/com/jetpack/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/jetpack/movieapp/navigation/MovieNavigation.kt
@@ -4,18 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.jetpack.movieapp.screens.DetailsScreen
+import com.jetpack.movieapp.screens.home.HomeScreen
 
 @Composable
 fun MovieNavigation() {
+
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = MovieScreens.HomeScreen.name
     ) {
+
         composable(MovieScreens.HomeScreen.name) {
             //here we pass where this should lead us to
-            //HomeScreen()
-
+            HomeScreen(navController)
         }
+
+        composable(MovieScreens.DetailsScreen.name) {
+            DetailsScreen(navController)
+        }
+
     }
+
 }

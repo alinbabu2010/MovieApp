ef3da08556fe9b33faa67c0c0cbcea17bcf7f9c9
Alin Babu <alinbabu2010@gmail.com>
2022-04-29 14:48:40 +0530

    Modify details screen composable

--- app/src/main/java/com/jetpack/movieapp/screens/DetalisScreen.kt
+++ app/src/main/java/com/jetpack/movieapp/screens/DetalisScreen.kt
@@ -1,13 +1,27 @@
 package com.jetpack.movieapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 
 @Composable
-fun DetailsScreen(navController: NavController) {
+fun DetailsScreen(navController: NavController, movieData: String?) {
 
-    Text(text = "Details Screen")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+        }
+    }
 
 }

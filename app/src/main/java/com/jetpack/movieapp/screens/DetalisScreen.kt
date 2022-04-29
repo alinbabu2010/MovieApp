7cc2b2307a61f743de755c36f9af10ed7f1b9940
Alin Babu <alinbabu2010@gmail.com>
2022-04-29 15:31:14 +0530

    Add TopAppbar and back navigation to details screen

--- app/src/main/java/com/jetpack/movieapp/screens/DetalisScreen.kt
+++ app/src/main/java/com/jetpack/movieapp/screens/DetalisScreen.kt
@@ -1,13 +1,13 @@
 package com.jetpack.movieapp.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -15,17 +15,36 @@ import androidx.navigation.NavController
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
 
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(topBar = {
+        TopAppBar(
+            backgroundColor = Color.Cyan,
+            elevation = 5.dp
         ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = { navController.popBackStack() }) {
-                Text(text = "Go Back")
+            Row(horizontalArrangement = Arrangement.Start) {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back Arrow")
+                }
+                Spacer(modifier = Modifier.width(100.dp))
+                Text(
+                    text = movieData.toString(),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+
+
+        }
+    }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
             }
         }
+
     }
 
 }

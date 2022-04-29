07dd8c8d72a069c02bf8c991485f6cadd52c0371
Alin Babu <alinbabu2010@gmail.com>
2022-04-29 14:29:14 +0530

    Add details screen

---
+++ app/src/main/java/com/jetpack/movieapp/screens/DetalisScreen.kt
@@ -0,0 +1,13 @@
+package com.jetpack.movieapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun DetailsScreen(navController: NavController) {
+
+    Text(text = "Details Screen")
+
+}

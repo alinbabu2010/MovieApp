34a79ab07aa130742725ef12c104e1767a98c8d6
Alin Babu <alinbabu2010@gmail.com>
2022-05-02 10:20:03 +0530

    Add moviesList using getMovies function and remove unused codes

--- app/src/main/java/com/jetpack/movieapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/jetpack/movieapp/ui/screens/HomeScreen.kt
@@ -1,22 +1,22 @@
 package com.jetpack.movieapp.ui.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.jetpack.movieapp.models.Movie
+import com.jetpack.movieapp.models.getMovies
 import com.jetpack.movieapp.ui.navigation.MovieScreens
+import com.jetpack.movieapp.ui.widgets.MovieRow
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -32,17 +32,8 @@ fun HomeScreen(navController: NavController) {
     }
 }
 
-
-val moviesList = listOf(
-    "Avatar",
-    "KGF",
-    "300",
-    "Avengers",
-    "Ruroni Kenshin"
-)
-
 @Composable
-fun MainContent(navController: NavController, movieList: List<String> = moviesList) {
+fun MainContent(navController: NavController, movieList: List<Movie> = getMovies(LocalContext.current)) {
     Column(
         modifier = Modifier.padding(start = 12.dp, end = 12.dp)
     ) {

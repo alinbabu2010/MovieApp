633e7dd9a07758270f739ce63e3d5e18ee2eb576
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 09:50:07 +0530

    Modify movie row widget to add image loading and more text composable

--- app/src/main/java/com/jetpack/movieapp/ui/widgets/Widgets.kt
+++ app/src/main/java/com/jetpack/movieapp/ui/widgets/Widgets.kt
@@ -2,10 +2,11 @@ package com.jetpack.movieapp.ui.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -13,9 +14,16 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.jetpack.movieapp.R
 import com.jetpack.movieapp.models.Movie
+import com.jetpack.movieapp.models.getMovies
 
 @Composable
 fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit) {
@@ -38,12 +46,35 @@ fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit) {
                     modifier = Modifier
                         .padding(12.dp)
                         .size(100.dp),
-                    shape = RectangleShape,
-                    elevation = 4.dp
+                    shape = CircleShape,
+                    elevation = 8.dp
                 ) {
-                    Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie image")
+//                    Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie image")
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(movie.images[0])
+                            .crossfade(true)
+                            .build(),
+                        placeholder = rememberVectorPainter(Icons.Default.AccountBox),
+                        contentDescription = stringResource(R.string.poster_description),
+                        contentScale = ContentScale.FillBounds
+                    )
                 }
-                Text(text = movie.title)
+                Column(Modifier.padding(4.dp)) {
+                    Text(text = movie.title, style = MaterialTheme.typography.h5)
+                    Text(
+                        text = stringResource(R.string.movie_director, movie.director),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                    Text(
+                        text = stringResource(R.string.movie_rating, movie.rating),
+                        style = MaterialTheme.typography.caption
+                    )
+                    Text(
+                        text = stringResource(R.string.movie_year, movie.year),
+                        style = MaterialTheme.typography.caption
+                    )
+                }
             }
         }
     }

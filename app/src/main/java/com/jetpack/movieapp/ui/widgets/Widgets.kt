e94fa73ec44e6aa4dcda1a85b01fda371299cbf6
Alin Babu <alinbabu2010@gmail.com>
2022-05-03 10:10:36 +0530

    Adding expanded capability on the card to show more info

--- app/src/main/java/com/jetpack/movieapp/ui/widgets/Widgets.kt
+++ app/src/main/java/com/jetpack/movieapp/ui/widgets/Widgets.kt
@@ -1,37 +1,53 @@
 package com.jetpack.movieapp.ui.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.jetpack.movieapp.R
 import com.jetpack.movieapp.models.Movie
 import com.jetpack.movieapp.models.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit) {
+fun MovieRow(
+    movie: Movie = getMovies(LocalContext.current)[0],
+    onItemClicked: (String) -> Unit = {}
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
-            .fillMaxWidth()
-            .height(130.dp),
+            .fillMaxWidth(),
+        //.height(130.dp),
         shape = RoundedCornerShape(CornerSize(16.dp)),
         elevation = 6.dp
     ) {
@@ -67,12 +83,40 @@ fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit) {
                         style = MaterialTheme.typography.subtitle1
                     )
                     Text(
-                        text = stringResource(R.string.movie_rating, movie.rating),
-                        style = MaterialTheme.typography.caption
-                    )
-                    Text(
                         text = stringResource(R.string.movie_year, movie.year),
                         style = MaterialTheme.typography.caption
+                    )
+                    AnimatedVisibility(visible = expanded) {
+                        Column {
+                            // Span text creation in compose
+                            Text(buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp
+                                    )
+                                ) {
+                                    append("Plot: ")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                ) {
+                                    append(movie.plot)
+                                }
+                            })
+                        }
+                    }
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                        contentDescription = stringResource(R.string.down_arrow),
+                        modifier = Modifier
+                            .size(25.dp)
+                            .clickable { expanded = !expanded },
+                        tint = Color.DarkGray
                     )
                 }
             }
